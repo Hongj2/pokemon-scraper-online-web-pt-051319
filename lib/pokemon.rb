72372e5567a3162f0d65202f2f1aaa5f1d67fe59
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-07-26 02:15:16 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+  attr_accessor :name, :type , :db
+  attr_reader :id
+  
+  def initialize(keywords)
+# WHYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY
+  end
+  
+  def self.save(name,type,db)
+    db.execute("INSERT INTO pokemons (name, type) 
+      VALUES(?, ?)", name, type)
+  end
+  
+  def self.find(db, num )
+    db.execute(sql, id)
+  end
+
 end
